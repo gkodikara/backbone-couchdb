3b15d9b0c6203a2b5c50c8df10747e01c8e1aaf5
Greg <gkodikara@gmail.com>
2013-04-01 16:42:51 +1100

    New ip

--- couchdb_connect.php
+++ couchdb_connect.php
@@ -1,11 +1,11 @@
-<script src="http://217.199.160.116:5585/_utils/script/json2.js"></script>
-<script src="http://217.199.160.116:5585/_utils/script/sha1.js"></script>
-<script src="http://217.199.160.116:5585/_utils/script/jquery.js?1.4.2"></script>
-<script src="http://217.199.160.116:5585/_utils/script/jquery.couch.js?0.11.0"></script>
-<script src="http://217.199.160.116:5585/_utils/script/jquery.dialog.js?0.11.0"></script>
+<script src="http://127.0.0.1:5585/_utils/script/json2.js"></script>
+<script src="http://127.0.0.1:5585/_utils/script/sha1.js"></script>
+<script src="http://127.0.0.1:5585/_utils/script/jquery.js?1.4.2"></script>
+<script src="http://127.0.0.1:5585/_utils/script/jquery.couch.js?0.11.0"></script>
+<script src="http://127.0.0.1:5585/_utils/script/jquery.dialog.js?0.11.0"></script>
 
 <script type="text/javascript">
-$.couch.urlPrefix = "http://217.199.160.116:5585";
+$.couch.urlPrefix = "http://127.0.0.1:5585";
 $.couch.info({
     success: function(data) {
         console.log(data);

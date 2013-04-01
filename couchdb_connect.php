6089893c09fe4894878040c9e1cf1ad4eb9d110a
Greg <gkodikara@gmail.com>
2013-04-01 23:49:52 +1100

    Change url

--- couchdb_connect.php
+++ couchdb_connect.php
@@ -3,11 +3,11 @@
   <head>
     <title>CouchDB jQuery Examples</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <script src="/_utils/script/json2.js"></script>
-    <script src="/_utils/script/sha1.js"></script>
-    <script src="/_utils/script/jquery.js?1.4.2"></script>
-    <script src="/_utils/script/jquery.couch.js?0.11.0"></script>
-    <script src="/_utils/script/jquery.dialog.js?0.11.0"></script>
+    <script src=":5585/_utils/script/json2.js"></script>
+    <script src=":5585/_utils/script/sha1.js"></script>
+    <script src=":5585/_utils/script/jquery.js?1.4.2"></script>
+    <script src=":5585/_utils/script/jquery.couch.js?0.11.0"></script>
+    <script src=":5585/_utils/script/jquery.dialog.js?0.11.0"></script>
 
 	<script type="text/javascript">
 		$.couch.urlPrefix = "http://crowdfit.gregkodikara.com:5585";

0e6b36c9fd1a7635220ee84e22ef5cd32c358009
Greg <gkodikara@gmail.com>
2013-04-01 23:44:38 +1100

    Change url

--- couchdb_connect.php
+++ couchdb_connect.php
@@ -1,15 +1,26 @@
-<script src="http://crowdfit.gregkodikara.com:5585/_utils/script/json2.js"></script>
-<script src="http://crowdfit.gregkodikara.com:5585/_utils/script/sha1.js"></script>
-<script src="http://crowdfit.gregkodikara.com:5585/_utils/script/jquery.js?1.4.2"></script>
-<script src="http://crowdfit.gregkodikara.com:5585/_utils/script/jquery.couch.js?0.11.0"></script>
-<script src="http://crowdfit.gregkodikara.com:5585/_utils/script/jquery.dialog.js?0.11.0"></script>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CouchDB jQuery Examples</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script src="/_utils/script/json2.js"></script>
+    <script src="/_utils/script/sha1.js"></script>
+    <script src="/_utils/script/jquery.js?1.4.2"></script>
+    <script src="/_utils/script/jquery.couch.js?0.11.0"></script>
+    <script src="/_utils/script/jquery.dialog.js?0.11.0"></script>
 
-<script type="text/javascript">
-$.couch.urlPrefix = "http://crowdfit.gregkodikara.com:5585";
-$.couch.info({
-    success: function(data) {
-        console.log(data);
-    }
-});
+	<script type="text/javascript">
+		$.couch.urlPrefix = "http://crowdfit.gregkodikara.com:5585";
+		$.couch.info({
+			success: function(data) {
+			    console.log(data);
+			}
+		});
 
-</script>
+	</script>
+
+
+  </head>
+  <body>
+  </body>
+</html>
